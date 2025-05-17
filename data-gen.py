cf13425853ce6a8e7710d2abcdd4f215b94f953e
Charles Lee <leecf2@miamioh.edu>
2025-05-16 20:12:28 -0400

    modified:   data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -14,8 +14,8 @@ def generate_test_data(num_contacts):
     for i in range(num_contacts):
         name = f'Test Name {i}'
         phone = f'123-456-789{i}'
-        t = random_address.real_random_address()
-        address = f'{t.get('address1')}'
+        t = random_address.real_random_address().get('address1')
+        address = f'{t}'
         db.execute('INSERT INTO contactss (name, phone, address) VALUES (?, ?, ?)', (name, phone, address))
     db.commit()
     print(f'{num_contacts} test contacts added to the database.')
